5aae5e14a51eba639c95233c431a54feab737fea
Chan, Danny <danny50610@yahoo.com.tw>
2018-10-01 10:35:59 +0800

    Install jshashes 完成 md5, sha1, sha256

--- src/renderer/components/Conversion.vue
+++ src/renderer/components/Conversion.vue
@@ -3,10 +3,10 @@
         <div class="row">
             <div class="col-8">
                 <h2>輸入</h2>
-                <textarea class="form-control" rows="5" v-model="inputData"></textarea>
+                <textarea class="form-control" rows="5" v-model="inputData" id="inputTextarea"></textarea>
                 <hr/>
                 <h2>輸出</h2>
-                <textarea class="form-control mb-2" rows="5" v-model="outputData" readonly></textarea>
+                <textarea class="form-control mb-2" rows="5" v-model="outputData" id="outputTextarea" readonly></textarea>
                 <div class="float-right">
                     <button type="button" class="btn btn-outline-dark">
                         <font-awesome-icon icon="copy"/>
@@ -34,7 +34,7 @@
                                 @click="urlDecode()">
                             URL Decode
                         </button>
-                        <button type="button" class="btn btn-info btn-block"
+                        <button type="button" class="btn btn-info btn-block" id="md5"
                                 @click="md5()">
                             MD5
                         </button>
@@ -54,6 +54,8 @@
 </template>
 
 <script>
+    const Hashes = require('jshashes');
+
     export default {
         name: 'Conversion',
         data() {
@@ -80,16 +82,13 @@
                 this.outputData = this.inputData;
             },
             md5() {
-                // TODO:
-                this.outputData = this.inputData;
+                this.outputData = new Hashes.MD5().hex(this.inputData);
             },
             sha1() {
-                // TODO:
-                this.outputData = this.inputData;
+                this.outputData = new Hashes.SHA1().hex(this.inputData);
             },
             sha256() {
-                // TODO:
-                this.outputData = this.inputData;
+                this.outputData = new Hashes.SHA256().hex(this.inputData);
             },
         },
     };
