b82739ff25a2b5281479eaf912179edf5bfd541e
Chan, Danny <danny50610@yahoo.com.tw>
2018-10-08 09:45:18 +0800

    提出 function 測試 url decode 功能

--- test/unit/specs/Conversion.spec.js
+++ test/unit/specs/Conversion.spec.js
@@ -14,45 +14,38 @@ describe('Conversion.vue', () => {
             .contain('輸入');
     });
 
+    function testButton(inputData, outputData, buttonId) {
+        const wrapper = shallowMount(Conversion);
+        wrapper.setData({
+            inputData,
+        });
+
+        wrapper.find(buttonId)
+            .trigger('click');
+
+        expect(wrapper.find('#outputTextarea').element.value)
+            .contains(outputData);
+    }
+
     const base64Data = 'Hello, illya';
     const base64edData = 'SGVsbG8sIGlsbHlh';
 
     it('測試 base64 encode 功能', () => {
-        const wrapper = shallowMount(Conversion);
-        wrapper.setData({
-            inputData: base64Data,
-        });
-
-        wrapper.find('#base64Encode')
-            .trigger('click');
-
-        expect(wrapper.find('#outputTextarea').element.value)
-            .contains(base64edData);
+        testButton(base64Data, base64edData, '#base64Encode');
     });
 
     it('測試 base64 decode 功能', () => {
-        const wrapper = shallowMount(Conversion);
-        wrapper.setData({
-            inputData: base64edData,
-        });
-
-        wrapper.find('#base64Decode')
-            .trigger('click');
-
-        expect(wrapper.find('#outputTextarea').element.value)
-            .contains(base64Data);
+        testButton(base64edData, base64Data, '#base64Decode');
     });
 
+    const urlData = 'ABCD!#$&\'()*+,/:;=?@[]0123 伊利雅好可愛';
+    const urledData = 'ABCD!%23%24%26\'()*%2B%2C%2F%3A%3B%3D%3F%40%5B%5D0123%20%E4%BC%8A%E5%88%A9%E9%9B%85%E5%A5%BD%E5%8F%AF%E6%84%9B';
+
     it('測試 url encode 功能', () => {
-        const wrapper = shallowMount(Conversion);
-        wrapper.setData({
-            inputData: 'ABCD!#$&\'()*+,/:;=?@[]0123 伊利雅好可愛',
-        });
+        testButton(urlData, urledData, '#urlEncode');
+    });
 
-        wrapper.find('#urlEncode')
-            .trigger('click');
-
-        expect(wrapper.find('#outputTextarea').element.value)
-            .contains('ABCD!%23%24%26\'()*%2B%2C%2F%3A%3B%3D%3F%40%5B%5D0123%20%E4%BC%8A%E5%88%A9%E9%9B%85%E5%A5%BD%E5%8F%AF%E6%84%9B');
+    it('測試 url decode 功能', () => {
+        testButton(urledData, urlData, '#urlDecode');
     });
 });
